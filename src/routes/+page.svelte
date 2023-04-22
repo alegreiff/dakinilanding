15c90260c79b33459864be7958246c65582e8fcf
Jaime de Greiff <alegreiff@gmail.com>
2023-04-22 10:37:25 -0500

    Inicio landing

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<div>
+	<h1>Studio Dakini</h1>
+	<h2>Transforma tu dolor</h2>
+</div>
+
+<style>
+	div {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		background-color: #b1bfd2;
+		height: 100vh;
+	}
+</style>
